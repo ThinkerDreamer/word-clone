2b038dd06ecf05c0df67a386453191e669f24deb
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-02-12 20:34:57 +0100

    use answer as prop instead of imported value

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,11 +3,15 @@ import { range } from "../../utils";
 import Guess from "../Guess";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((index) => (
-        <Guess key={index} value={guesses[index]} />
+        <Guess
+          key={index}
+          value={guesses.length > 0 ? guesses[index] : undefined}
+          answer={answer}
+        />
       ))}
     </div>
   );
