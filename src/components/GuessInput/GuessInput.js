bca3dede4d00e8a3f41137253167b7e96aefa7a2
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-02-12 15:07:22 +0100

    make guess log in the console

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,25 +2,28 @@ import React from "react";
 
 function GuessInput() {
   const [guess, setGuess] = React.useState("");
-  console.log(guess);
 
   function handleInput(event) {
     event.preventDefault();
-    console.log("Hello");
+    console.log(guess);
     setGuess("");
   }
   return (
-    <form className="guess-input-wrapper">
-      <label htmlFor="guess-input" onSubmit={(event) => handleInput(event)}>
-        Enter Guess:{" "}
-      </label>
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => handleInput(event)}
+    >
+      <label htmlFor="guess-input">Enter Guess: </label>
       <input
         type="text"
         className="guess-input"
         value={guess}
+        required={true}
         id="guess-input"
         minLength={5}
         maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
         onChange={(event) => setGuess(event.target.value.toUpperCase())}
       ></input>
     </form>
